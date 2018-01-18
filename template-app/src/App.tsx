6776f24baed699be3140364fc337d3e220c499c4
Christopher Diggins <christopherd@clemex.com>
2018-01-18 17:20:09 -0500

    Example of a simple stateless TypeScript component as an arrow function without properties.

--- template-app/src/App.tsx
+++ template-app/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import Child from './Child';
 
 const logo = require('./logo.svg');
 
@@ -14,6 +15,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Child />
       </div>
     );
   }
---
+++ template-app/src/Child.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+// A simple stateless component with no properties.
+const Child = () => (
+    <div>
+      I am a simple child!
+   </div>
+  );
+
+export default Child;
