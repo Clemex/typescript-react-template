487b09ef961a35d12edf07e7b9af1e11286e4160
Christopher Diggins <christopherd@clemex.com>
2018-01-26 13:10:42 -0500

    WIP.

--- template-app/src/index.js
+++ template-app/src/index.js
@@ -6,3 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
