6265cea423b4239cc156fd5c5883d1b65cb4b50b
Christopher Diggins <christopherd@clemex.com>
2018-01-26 13:08:09 -0500

    A wrapper for making a pure component around another component.

--- template-app/src/components/demo/HOC.tsx
+++ template-app/src/components/demo/HOC.tsx
@@ -3,49 +3,19 @@
 
 import * as React from 'react';
 
-// The type of a render function for components that render based on properties and internal state
-export interface RenderFunc<P, S> {
-    (props: P, state: S) : React.ReactNode
-}
-
-// A Pure stateful component sounds 
-export class PureStatefulComponent<P, S> extends React.PureComponent<P|null, S> 
-{        
-    // A function
-    readonly renderFunc: RenderFunc<P, S>;
-
-    // Pass in the the name of the constructor, the initial state, and the function that renders from state
-    constructor(initialState: S, renderFunc: RenderFunc<P, S> ) {
-        super(null);
-        this.renderFunc = renderFunc;
-        this.setState(initialState);
-    }
-    
-    // Render function 
-    render(): React.ReactNode {
-        return this.renderFunc(this.props, this.state);
+// This function will convert a function into a pure component. SFC (functions) used as components 
+// in React are not efficient. We don't want to have two different syntaxes. 
+// https://dev.to/danhomola/react-higher-order-components-in-typescript-made-simple
+// The advantage of using a function is that it removes the temptation to make errors. 
+export function pureSFC<P extends {}>(Component: React.StatelessComponent<P>): React.ComponentClass<P>
+{
+    return class PureSFC extends React.PureComponent<P> 
+    {            
+        // Render function 
+        render(): React.ReactNode {
+            return <Component {...this.props}/>;
+        }
     }
 }
 
-// Given the class of the property, and initial state, and a render function, generates a stateful pure component
-export function MakeStatefulComponent<P, S>(
-    initialState: S, 
-    renderFunc: RenderFunc<P, S>): PureStatefulComponent<P, S> 
-{
-    return new PureStatefulComponent(initialState, renderFunc);
-}
 
-// Given the class of the property, and initial state, and a render function, generates a stateful pure component
-export function MakeStatelessComponent<P>(
-    renderFunc: RenderFunc<P, {}>): PureStatefulComponent<P, {}> 
-{
-    return new PureStatefulComponent({}, renderFunc);
-}
-
-// Given an initial state, and a render function, generates a pure stateless component
-export function MakeProplessComponent<S>(
-    initialState: S, 
-    renderFunc: RenderFunc<{}, S>): PureStatefulComponent<{}, S> 
-{
-    return new PureStatefulComponent(initialState, renderFunc);
-}
